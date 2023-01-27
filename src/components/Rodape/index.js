77bbff43b823408fb06261a41bb32023fb302028
Bruna <brunadias_30@hotmail.com>
2023-01-26 23:00:19 -0300

    rodape

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Rodape.module.css'
+
+export default function Rodape() {
+    return (
+        <footer className={style.rodape}>
+            <h2>Desenvolvido por Alura.</h2>
+        </footer>
+    )
+}
