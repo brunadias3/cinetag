7290c2a4693a4b98cca1c20554afc13024bbc822
Bruna <brunadias_30@hotmail.com>
2023-01-27 21:18:07 -0300

    utilizando de apis

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,14 +1,25 @@
 import Banner from 'components/Banner'
 import Titulo from 'components/Titulo'
 import Card from 'components/Card'
-import videos from 'json/db.json'
+//import videos from 'json/db.json'
 import style from './Inicio.module.css'
+import { useEffect, useState } from 'react';
 
 export default function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/monicahillman/cinetag-api/videos')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setVideos(dados)
+      })
+  }, [])
+
   return (
     <>
       <Banner imagem="home" />
-      
+
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,18 +1,32 @@
 import Banner from 'components/Banner'
 import style from './Player.module.css'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Titulo from 'components/Titulo'
 import videos from 'json/db.json'
 import { useParams } from 'react-router-dom'
 import NaoEncontrada from 'pages/NaoEncontrada'
 
 export default function Player() {
-    const parametros = useParams();
-    const video = videos.find((video) => {
-        return video.id === Number(parametros.id)
-    })
 
-    if(!video) {
+    //variavel para pegar os dados json por api:
+    const [video, setVideo] = useState([]);
+
+    const parametros = useParams();
+
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/monicahillman/cinetag-api/videos?id=${parametros.id}`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideo(...dados)
+            })
+    }, [])
+
+    //com o arquivo db.json local:
+    // const video = videos.find((video) => {
+    //     return video.id === Number(parametros.id)
+    // })
+
+    if (!video) {
         return <NaoEncontrada />
     }
     return (
